7addc55f76296618095e60d8a3c591eb7d1f5eb2
Emily Pena <emilyapena@gmail.com>
2020-03-12 15:33:08 -0600

    Navbar, pages up and running

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import { Container } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Navbar />
+    <Container>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/About' component={About} />
+    </Container>
+  </>
+)
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Menu, } from 'semantic-ui-react';
+import { Link, } from 'react-router-dom';
+
+export default class NavBar extends Component {
+  state = {
+    activeItem: 'home'
+  };
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  render() {
+    const activeItem = this.state;
+    return (
+      <Menu inverted>
+        <Link to="/">
+        <Menu.Item
+          as = 'div'
+          name='home'
+          active={activeItem === 'home'}
+          onClick={this.handleItemClick}
+        >
+          Home
+      </Menu.Item>
+      </Link>
+      <Link to="/About">
+        <Menu.Item
+          as = 'div'
+          name='about'
+          active={activeItem === 'about'}
+          onClick={this.handleItemClick}
+        >
+          About
+      </Menu.Item>
+      </Link>
+      <Link to="/App">
+        <Menu.Item
+          as = 'div'
+          name='app'
+          active={activeItem === 'app'}
+          onClick={this.handleItemClick}
+        >
+          Virus
+      </Menu.Item>
+      </Link>
+    </Menu >
+    )
+  };
+};
